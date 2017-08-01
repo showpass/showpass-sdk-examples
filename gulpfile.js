469d48c446f67d504b98885bfd925f536ec51fb9
Marc Eshleman <marc@myshowpass.com>
2017-08-01 17:34:08 -0600

    sdk | install starter theme

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,14 @@ var gulp = require('gulp'),
         connect = require('gulp-connect');
 
 gulp.task('serve', function() {
-     connect.server();
+     //connect.server();
+     /*
+      *
+      * Use for connecting to network ip to test on mobile devices*/
+      connect.server({
+          host : '192.168.1.21',
+          livereload: true
+      });
 });
 
 gulp.task('default', ['serve']);
