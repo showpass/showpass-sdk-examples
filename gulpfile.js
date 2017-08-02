e0530982d725dae26a7415ed894e572ec3af0f1f
Marc Eshleman <marc@myshowpass.com>
2017-08-02 09:31:25 -0600

    gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ var gulp = require('gulp'),
         connect = require('gulp-connect');
 
 gulp.task('serve', function() {
-     //connect.server();
+     connect.server();
      /*
       *
-      * Use for connecting to network ip to test on mobile devices*/
+      * Use for connecting to network ip to test on mobile devices
       connect.server({
-          host : '192.168.1.21',
+          host : '192.168.1.20',
           livereload: true
-      });
+      });*/
 });
 
 gulp.task('default', ['serve']);
